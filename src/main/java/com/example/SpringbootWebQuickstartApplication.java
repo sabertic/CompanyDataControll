71f81b0f07ed36e2e1c0ec33351df614263be7b6
test <abc@163.com>
2023-08-07 11:18:56 +0800

    project1

---
+++ src/main/java/com/example/SpringbootWebQuickstartApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebQuickstartApplication {
+    public static void main (String[] args) {
+        SpringApplication.run (SpringbootWebQuickstartApplication.class, args);
+    }
+    
+}
---
+++ src/main/java/com/example/controller/project.java
@@ -0,0 +1,55 @@
+package com.example.controller;
+
+import com.example.Project.User;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.Array;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class project {
+    @RequestMapping("" +
+            "")
+    public String simpleparam (String username, Integer age) {
+        System.out.println (username + "   " + age); return "ok";
+    }
+    
+    @RequestMapping("/simplepojo")
+    public String simplepojo (User user) {
+        System.out.println (user); return "ok";
+    }
+    
+    @RequestMapping("/complexepojo")
+    public String complexpojo (User user) {
+        System.out.println (user); return "ok";
+    }
+    
+    @RequestMapping("/arrayparam")
+    public String arrayparam (String[] hobby) {
+        System.out.println (Arrays.toString (hobby)); return "ok";
+    }
+    
+    @RequestMapping("/listparam")
+    public String listparam (@RequestParam List<String> hobby) {
+        System.out.println (hobby);
+        return "ok";
+    }
+    
+    @RequestMapping("/dataparam")
+    public String dataparam (@DateTimeFormat (pattern = "yy-MM-dd  HH-mm-ss")LocalDateTime localDateTime) {
+        System.out.println (localDateTime);
+        return "ok";
+    }
+    
+    @RequestMapping("/json")
+    public String json(@RequestBody User user) {
+        System.out.println (user);
+        return "ok";
+    }
+}
